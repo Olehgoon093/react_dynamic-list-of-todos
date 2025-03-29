e1138e544ed19a7b418fa81b447e75e0fb3a6477
Oleh <oleglapay2008@gmail.com>
2025-03-29 02:21:39 +0200

    Solution

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -1,8 +1,27 @@
-export const TodoFilter = () => (
-  <form className="field has-addons">
+import { TodoStatus } from '../../TodoStatus';
+
+type Props = {
+  onSelectStatus: (status: TodoStatus) => void;
+  selectedStatus: string;
+  searchValue: string;
+  onSearchValueChange: (searchValue: string) => void;
+  onSearchValueClear: () => void;
+};
+
+export const TodoFilter = ({
+  onSelectStatus,
+  selectedStatus,
+  searchValue,
+  onSearchValueChange,
+}: Props) => (
+  <form className="field has-addons" onClick={event => event.preventDefault()}>
     <p className="control">
       <span className="select">
-        <select data-cy="statusSelect">
+        <select
+          data-cy="statusSelect"
+          onChange={event => onSelectStatus(event.target.value as TodoStatus)}
+          value={selectedStatus}
+        >
           <option value="all">All</option>
           <option value="active">Active</option>
           <option value="completed">Completed</option>
@@ -16,6 +35,8 @@ export const TodoFilter = () => (
         type="text"
         className="input"
         placeholder="Search..."
+        onChange={event => onSearchValueChange(event.target.value)}
+        value={searchValue}
       />
       <span className="icon is-left">
         <i className="fas fa-magnifying-glass" />
@@ -23,7 +44,12 @@ export const TodoFilter = () => (
 
       <span className="icon is-right" style={{ pointerEvents: 'all' }}>
         {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-        <button data-cy="clearSearchButton" type="button" className="delete" />
+        <button
+          data-cy="clearSearchButton"
+          type="button"
+          className="delete"
+          onClick={() => onSearchValueChange('')}
+        />
       </span>
     </p>
   </form>
